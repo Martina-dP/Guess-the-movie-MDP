ae8f68bbd8f9daff954c1f0fef278f5866c2883d
Martina del Pino <martudelpino@gmail.com>
2024-12-26 18:00:49 -0300

    menu de opciones

--- src/delPinoMartinaMain.java
+++ src/delPinoMartinaMain.java
@@ -1,5 +1,54 @@
-public class ApellidoNombreMain {
+import java.util.Scanner;
+
+public class delPinoMartinaMain {
+
+    Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        delPinoMartinaMain program = new delPinoMartinaMain();
+        program.inicio();
     }
+
+    public void inicio() {
+    }
+
+    public void Menu() {
+        int opcion ;
+        boolean end = false;
+
+        System.out.println("Bienvenido a Guess the movie : " );
+
+        while (true) {
+
+            System.out.println(" Seleccione una opción: ");
+            System.out.println("1 = Adivinar una letra");
+            System.out.println("2 = Adivinar el titulo de la pelicula");
+            System.out.println("3 = Salir");
+
+            opcion = input.nextInt();
+            input.nextLine();
+
+            switch (opcion) {
+                case 1: {
+                    System.out.println("Adivinar una letra" );
+                    break;
+                }
+                case 2: {
+                    System.out.println("Adivinar el titulo de la pelicula");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Salir");
+                    end = true;
+                    return;
+                }
+                default: {
+                    System.out.println("Opción no válida. Por favor, elige 1, 2, o 3");
+                    break;
+                }
+            }
+        }
+    }
+
+
 }
